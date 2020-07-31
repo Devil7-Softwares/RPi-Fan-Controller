85476690ac8a4d1defa8a06e02ac84a8032b4efc
Devil7DK <dineshthangavel47@gmail.com>
2020-07-31 20:16:42 +0530

    Setup syslog for logging

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,19 @@
-#include <iostream>
+#include <stdlib.h>
+#include <syslog.h>
+
+#define DAEMON_NAME "fcdaemon"
 
 using namespace std;
 
-int main(int argc, char *argv[]) {
-    cout << "Hello World!" << endl;
+int main (int argc, char *argv[]) {
+    // Setup logging
+    setlogmask (LOG_UPTO (LOG_NOTICE));
+    openlog (DAEMON_NAME, LOG_CONS | LOG_PID | LOG_NDELAY, LOG_DAEMON); // For more info: https://linux.die.net/man/3/syslog
 
-    return 0;
+    syslog(LOG_NOTICE, "RPi fan controller daemon started!");
+
+    syslog(LOG_NOTICE, "RPi fan controller daemon stopped!");
+
+    closelog();
+    return(EXIT_SUCCESS);
 }
