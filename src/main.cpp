7a43f6ebe10dd2479c9d85063792c7b0271720b0
Devil7DK <dineshthangavel47@gmail.com>
2020-07-31 20:50:38 +0530

    Add clang format config and format existing files

--- src/main.cpp
+++ src/main.cpp
@@ -5,15 +5,15 @@
 
 using namespace std;
 
-int main (int argc, char *argv[]) {
+int main(int argc, char *argv[]) {
     // Setup logging
-    setlogmask (LOG_UPTO (LOG_NOTICE));
-    openlog (DAEMON_NAME, LOG_CONS | LOG_PID | LOG_NDELAY, LOG_DAEMON); // For more info: https://linux.die.net/man/3/syslog
+    setlogmask(LOG_UPTO(LOG_NOTICE));
+    openlog(DAEMON_NAME, LOG_CONS | LOG_PID | LOG_NDELAY, LOG_DAEMON); // For more info: https://linux.die.net/man/3/syslog
 
     syslog(LOG_NOTICE, "RPi fan controller daemon started!");
 
     syslog(LOG_NOTICE, "RPi fan controller daemon stopped!");
 
     closelog();
-    return(EXIT_SUCCESS);
+    return (EXIT_SUCCESS);
 }
